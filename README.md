777b850cf214299e5f5a69956cee9b2306ab054e
gershgoriNN <189793370+gershgoriNN@users.noreply.github.com>
2024-12-18 17:12:18 +0100

    add GLOB to CMakeLists

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-Direct setup todos
-- [x] Add first external dependency
-- [x] Add script to generate compilation database using modern cmake
-- [ ] add clang-tidy
-
 ## Formatting
 based on clang-format
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
 #include <cpr/cpr.h>
 #include <iostream>
+#include "ex.hh"
 
 int main(int argc, char** argv) {
     cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
                       cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
                       cpr::Parameters{{"anon", "true"}, {"key", "value"}});
     std::cout << r.status_code << std::endl;                  // 200
+    f();
     return 0;
 }
