6b64270b1b85e7e0ec393361239f6536e1671ced
gershgoriNN <189793370+gershgoriNN@users.noreply.github.com>
2024-12-18 16:07:07 +0100

    add cpr

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+Direct setup todos
+- [x] Add first external dependency
+- [ ] Add script to generate compilation database using modern cmake
+- [ ] add clang-tidy
+
 ## Formatting
 based on clang-format
--- src/main.cc
+++ src/main.cc
@@ -1,2 +1,10 @@
+#include <cpr/cpr.h>
 #include <iostream>
-int main() { std::cout << "Hello, World!" << std::endl; }
+
+int main(int argc, char** argv) {
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    std::cout << r.status_code << std::endl;                  // 200
+    return 0;
+}
